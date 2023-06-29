fd549b5317e19a311b4261ddee419ed4c5b2a972
Tom DeMay <57812487+tomdemay@users.noreply.github.com>
2023-06-29 00:15:31 -0400

    Adding cover letter to actual web page and some formatting tweaks (again)

--- script.js
+++ script.js
@@ -4,22 +4,24 @@ window.addEventListener('beforeprint', function() {
     for (var i = 0; i < detailsElements.length; i++) {
       detailsElements[i].setAttribute('open', '');
     }
-  });
-  
-//   // Collapse all <details> elements after printing
-//   window.addEventListener('afterprint', function() {
-//     var detailsElements = document.getElementsByTagName('details');
-//     for (var i = 0; i < detailsElements.length; i++) {
-//       detailsElements[i].removeAttribute('open');
-//     }
-//   });
+});
 
-  
-// // Expand all <details> elements on page load
-// window.addEventListener('DOMContentLoaded', function() {
-//     var detailsElements = document.getElementsByTagName('details');
-//     for (var i = 0; i < detailsElements.length; i++) {
-//       detailsElements[i].setAttribute('open', '');
-//     }
-//   });
-  
+const checkbox = document.getElementById('show-cover-letter-checkbox');
+const checkboxLink = document.querySelector('.show-cover-letter-label-checkbox')
+const togglenav = document.querySelector('.toggle-nav');
+const coverLetter = document.querySelector('.cover-letter');
+
+checkboxLink.addEventListener('click', function () {
+    checkbox.checked = !checkbox.checked;
+    checkbox.dispatchEvent(new Event('change'));
+});
+
+checkbox.addEventListener('change', function() {
+    togglenav.checked = false;
+    if (this.checked) {
+        coverLetter.style.display = 'block';
+    } else {
+        coverLetter.style.display = 'none';
+    }
+});
+
